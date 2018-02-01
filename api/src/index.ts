333431c51e9be91d528473b15d72364b292fd3f4
Matt Breeden <mbreeden@graysail.com>
2018-02-01 17:19:38 -0500

    attempt https with certs as is

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,19 +1,37 @@
 import * as http from 'http';
+import * as https from 'https';
 import * as debug from 'debug';
-
+import * as fs from 'fs';
 import Server from './server';
 
 //debug('ts-express:server');
 const server = new Server();
 const port = normalizePort(4000);
+const sport = normalizePort(8443);
 //Server.set('port', port);
 
 console.log(`Server listening on port ${port}`);
+console.log("SERVER IS IN",process.env.);
+/*
+--cert /sapien/certificates/fullchain.pem --key /sapien/certificates/privkey.pem
+*/
+if(fs.existsSync('/sapien/certificates/privkey.pem')){
+  var privateKey  = fs.readFileSync('/sapien/certificates/privkey.pem', 'utf8').toString();
+  var certificate = fs.readFileSync('/sapien/certificates/fullchain.pem', 'utf8').toString();
+  var credentials = {key: privateKey, cert: certificate};
+  const httpsServer = https.createServer(credentials, server.app);
+  httpsServer.listen(sport);
+  httpsServer.on('error', onError);
+  httpsServer.on('listening', onListening);
+}
 
 const httpServer = http.createServer(server.app);
 httpServer.listen(port);
 httpServer.on('error', onError);
 httpServer.on('listening', onListening);
+
+
+
 function normalizePort(val: number|string): number|string|boolean {
   let port: number = (typeof val === 'string') ? parseInt(val, 10) : val;
   if (isNaN(port)) return val;
