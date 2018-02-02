ae1b80ee2e19ab1726bd8ac51869b807cc9a41f4
Matt Breeden <mbreeden@graysail.com>
2018-02-02 11:29:55 -0500

    Removed setting Access-Control-Allow-Credentials to true for websocket testing

--- api/src/server.ts
+++ api/src/server.ts
@@ -120,7 +120,7 @@ export default class Server {
 
             res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
-            res.header('Access-Control-Allow-Credentials', 'true');
+            //res.header('Access-Control-Allow-Credentials', 'true');
             next();
         });
         this.app.use(cors());
