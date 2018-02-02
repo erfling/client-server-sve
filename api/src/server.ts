67fe118be9db6510df05b478fc18069ec6eb12e4
Matt Breeden <mbreeden@graysail.com>
2018-02-02 12:10:29 -0500

    explicitly set Access-Control-Allow-Credentials

--- api/src/server.ts
+++ api/src/server.ts
@@ -120,7 +120,7 @@ export default class Server {
 
             res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-            //res.header('Access-Control-Allow-Credentials', 'true');
+            res.header('Access-Control-Allow-Credentials', 'false');
             next();
         });
         this.app.use(cors());
