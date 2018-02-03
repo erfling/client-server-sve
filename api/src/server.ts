960940200ae44522ec829cf6641f9ecae3c080f8
Matt Breeden <mbreeden@graysail.com>
2018-02-03 10:49:01 -0500

    stop with drive for now

--- api/src/server.ts
+++ api/src/server.ts
@@ -191,7 +191,7 @@ export default class Server {
             teams.forEach((t:ITeam) => {
                 //console.log()
                 //if(!this.gameSockets.has(t.Slug)){
-                this.sheets.subscribeToDriveResource("test")
+                //this.sheets.subscribeToDriveResource("test")
                 var teamSocket = this.io.of(t.Slug);
                 
                 this.gameSockets.set(t.Slug, teamSocket);
