2a14b1812c2ea2e77c03159bc5edc9a89a772a89
Matt Breeden <mbreeden@graysail.com>
2018-02-17 20:16:51 -0500

    Begin form styling

--- app/src/components/form-elements/WaterForm.tsx
+++ app/src/components/form-elements/WaterForm.tsx
@@ -55,56 +55,105 @@ class WaterFormWrapper extends React.Component<WaterFormProps, { warning:string 
 
     render(){
         return <form ref="waterForm" id="waterForm">
-                <FormItem>
-                    <label>Agriculture</label>
-                    <Field
-                        name="agriculture"
-                        component={SelectWrapper}
-                        validate={this.selectChanged}
-                    > 
-                        <option>No Water</option>
-                        <option value="SOME">Some Water</option>
-                        <option value="ALL">All Water</option>
-                    </Field>      
-                </FormItem>
-                <FormItem>
-                    <label>Healthcare</label>
-                    <Field
-                        name="healthcare"
-                        component={SelectWrapper}
-                        validate={this.selectChanged}
-                    > 
+                <div className="form-wrapper">
+                    <FormItem>
+                        <h3>The Case for Agriculture</h3>
+                        <div>
+                            <p>Population growth increases food production demand to over 80% of available water</p>
+                            <p>Diverted water leads to sourcing shifts from surface water to groundwater</p>
+                            <p>Groundwater recovery is expensive and requires &gt;20 times the energy to divert</p>
+                            <p>Fertilisers and pesticides which require intense water usage cannot be used</p>
+                            <p>Irrigation is unsustainable</p>
+                            <p>Yields drop</p>
+                            <p>Crops fail as drought increases</p>
+                            <p>Food production stops on previously arable land</p>
+                        </div>
+                        <label>Assign Water to Agriculture</label>
+                        <Field
+                            name="agriculture"
+                            component={SelectWrapper}
+                            validate={this.selectChanged}
+                        > 
                             <option>No Water</option>
                             <option value="SOME">Some Water</option>
                             <option value="ALL">All Water</option>
-                    </Field>
-                </FormItem>
-                <FormItem>
-                    <label>Industry</label>
-                    <Field
-                        name="industry"
-                        component={SelectWrapper}
-                        validate={this.selectChanged}
-                    > 
-                            <option>No Water</option>
-                            <option value="SOME">Some Water</option>
-                            <option value="ALL">All Water</option>
-                    </Field>    
-                </FormItem>
-                <FormItem>
-                    <label>Government</label>
-                    <Field
-                        name="government"
-                        component={SelectWrapper}
-                        validate={this.selectChanged}
-                    > 
-                            <option>No Water</option>
-                            <option value="SOME">Some Water</option>
-                            <option value="ALL">All Water</option>
-                    </Field>      
-                </FormItem>
+                        </Field>      
+                    </FormItem>
+                </div>
+                <div className="form-wrapper">
+                    <FormItem>
+                        <h3>The Case for Healthcare</h3>
+                        <div className="form-info">
+                            <p>Growing populations place increased demand on the healthcare services</p>
+                            <p>Clean water supply failure leads to the use of rainwater and other 'dirty' water sources</p>
+                            <p>Sanitation failures lead to massive spikes in infection for those in hospital</p>
+                            <p>Water borne disease increases</p><p>Newborns are particularly susceptible to infection and sepsis</p>
+                            <p>Infant mortality significantly increases</p><p>Women stop coming to the hospital to give birth</p>
+                            <p>Increases in mortality caused by other birth complications hospitals can treat</p>
+                            <p>Adult populations at greater risk of infections which originate in the hospital like cholera outbreaks</p>
+                            <p>Hospitals are no longer safe and incubate disease</p>
+                        </div>
+                        <label>Assign Water to Healthcare</label>
+                        <Field
+                            name="healthcare"
+                            component={SelectWrapper}
+                            validate={this.selectChanged}
+                        > 
+                                <option>No Water</option>
+                                <option value="SOME">Some Water</option>
+                                <option value="ALL">All Water</option>
+                        </Field>
+                    </FormItem>
+                </div>
+                <div className="form-wrapper">
+                    <FormItem>
+                        <h3>The Case for Industry</h3>
+                        <div>
+                            <p>Larger populations increase demand for water intensive production</p>
+                            <p>As water becomes scarce degraded water is used impacting product quality</p>
+                            <p>Production and output fall as costs escalate</p>
+                            <p>Financial pressure leads to headcount decrease</p>
+                            <p>Debt spirals and banks refuse to lend as revenues fall</p>
+                            <p>Companies collapse infecting the banking system which has unsustainable debt levels across multiple industries</p>
+                        </div>
+                        <label>Assign Water to Industry</label>
+                        <Field
+                            name="industry"
+                            component={SelectWrapper}
+                            validate={this.selectChanged}
+                        > 
+                                <option>No Water</option>
+                                <option value="SOME">Some Water</option>
+                                <option value="ALL">All Water</option>
+                        </Field>    
+                    </FormItem>
+                </div>
+
+                <div className="form-wrapper">
+                    <FormItem>
+                        <h3>The Case for Government</h3>
+                        <div>
+                            <p>Growing populations require water delivered by local government in regional areas</p>
+                            <p>Water supply shortages lead to imposed restrictions on personal use to below 25 litres per day</p>
+                            <p>Restrictions are insufficient and impact rural areas dramatically stopping supply completely in some areas</p>
+                            <p>Mass migration begins as millions head for cities which have more reliable source water</p>
+                            <p>Refugee crisis and civil unrest lead to mass anti-government demonstrations</p>
+                            <p>Military occupies streets</p><p>Governments are toppled after a state of emergency is introduced</p>
+                        </div>
+                        <label>Assign Water to Government</label>
+                        <Field
+                            name="government"
+                            component={SelectWrapper}
+                            validate={this.selectChanged}
+                        > 
+                                <option>No Water</option>
+                                <option value="SOME">Some Water</option>
+                                <option value="ALL">All Water</option>
+                        </Field>      
+                    </FormItem>
+                </div>
                 <Button className="game-button" onClick={e => this.props.handleSubmit(e)} disabled={this.props.invalid}>Distrubute Water {this.props.Submitting && <Icon type="loading"/>}</Button>
-                {this.props.invalid && <Alert message="There is only enough water to give some to give all of it to one sector, or some of it to two sectors." type="error"></Alert>}
+                {this.props.invalid && <Alert style={{marginTop:'15px'}} message="There is only enough water to give some to give all of it to one sector, or some of it to two sectors." type="error"></Alert>}
             </form>
                        
     }
