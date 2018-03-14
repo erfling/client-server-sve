5f78ac5277870cf874e89a816aa17c06caeca4e3
Matt Breeden <breeden@gnuidea.net>
2018-03-14 15:59:25 -0400

    authenticate db with admin source

--- api/src/AppServer.ts
+++ api/src/AppServer.ts
@@ -475,7 +475,7 @@ export default class AppServer
      */
     public config(): void {
         console.log("CONFIG CALLED")
-        const MONGO_URI: string = 'mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/admin';
+        const MONGO_URI: string = 'mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/express-boilerplate?authSource=admin';
         mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
         //mongoose.connect('mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/express-boilerplate');
 
