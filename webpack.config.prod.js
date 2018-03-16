598f0430da137203a6eb4ef7a20bc6011beda869
Matt Breeden <breeden@gnuidea.net>
2018-03-16 16:53:28 -0400

    more filepath wierdness

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -15,7 +15,7 @@ module.exports = {
     devtool: 'source-map',
     output: {
       path: ROOT_PATH +  '/dist',
-      publicPath: './dist',
+      publicPath: './dist/',
       filename: '[name].bundle.js'
     },
     module:{
