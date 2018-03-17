16a657e9ce6255b9ad36e320e9e74af9f5df727f
Matt Breeden <breeden@gnuidea.net>
2018-03-17 19:41:29 -0400

    begin ios detection

--- api/src/AppServer.ts
+++ api/src/AppServer.ts
@@ -495,8 +495,9 @@ export default class AppServer
 
         this.publicApp.use((req, res, next) => {
             console.log("request came from ", req.headers['user-agent']);
-            const agent = req.headers['user-agent'];
-            if(!agent){
+            const agent = <string>req.headers['user-agent'];
+            var isIOS = agent.match('[iPad|iPhone|iphone|iPod]')
+            if(isIOS){
                 res.sendFile("/sapien/client-server-sve/api/src/no-support.html")
             } else {
                 next();
