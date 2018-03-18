0f2863c02c3be357b22bef04936300dfbf268b2e
Matt Breeden <breeden@gnuidea.net>
2018-03-17 20:20:48 -0400

    comment

--- api/src/AppServer.ts
+++ api/src/AppServer.ts
@@ -494,7 +494,7 @@ export default class AppServer
         mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
         //mongoose.connect('mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/express-boilerplate');
 
-
+        //IOS verion interception
         this.publicApp.use((req, res, next) => {
             console.log("request came from ", req.headers['user-agent']);
             const agent = <string>req.headers['user-agent'];
