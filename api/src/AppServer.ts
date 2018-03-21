3cfde59fb7e6e13d8ca6446444b0a32a7f76a707
Matt Breeden <breeden@gnuidea.net>
2018-03-21 17:03:27 -0400

    change db

--- api/src/AppServer.ts
+++ api/src/AppServer.ts
@@ -522,7 +522,7 @@ export default class AppServer
      */
     public config(): void {
         console.log("CONFIG CALLED")
-        const MONGO_URI: string = 'mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/express-boilerplate?authSource=admin';
+        const MONGO_URI: string = 'mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/planetendeavor-master?authSource=admin';
         mongoose.connect(MONGO_URI || process.env.MONGODB_URI);
         //mongoose.connect('mongodb://mbreeden:MUfC9ex6CWRwktBf@localhost:27017/express-boilerplate');
 
