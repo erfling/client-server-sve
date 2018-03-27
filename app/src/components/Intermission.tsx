c2bf34870016709c2a6845993311d30b77a4fd81
Matt Breeden <breeden@gnuidea.net>
2018-03-26 22:18:45 -0400

    undo

--- app/src/components/Intermission.tsx
+++ app/src/components/Intermission.tsx
@@ -41,9 +41,9 @@ export class Intermission extends React.Component<IntermissionProps, { Now: any,
 
         var m = Math.floor(time % 3600 / 60);
         var s = Math.floor(time % 3600 % 60);
-        
-        var mDisplay = m/1000 > 9 ? m/1000 + ":" : "0" + m/1000 + ":";
-        var sDisplay = s/1000 > 9 ? s/1000 : "0" + s/1000;
+
+        var mDisplay = m > 9 ? m + ":" : "0" + m + ":";
+        var sDisplay = s > 9 ? s : "0" + s;
 
         return mDisplay + sDisplay;
     }
@@ -64,13 +64,11 @@ export class Intermission extends React.Component<IntermissionProps, { Now: any,
         if (this.state) {
             var now = Date.now();
             console.log(this.state.TwentyMinutesFromNow, this.state.TwentyMinutesFromNow - now);
-            
+            let time = this.secondsToTime((this.state.TwentyMinutesFromNow - now));
+            this.setState(Object.assign({}, this.state, { Now: time }))
             if (this.state.TwentyMinutesFromNow - now <= 0) {
                 clearInterval(this.state.Timer);
                 this.setState({ TimesUp: true, Now: "00:00" });
-            } else {
-                let time = this.secondsToTime((this.state.TwentyMinutesFromNow - now));
-                this.setState(Object.assign({}, this.state, { Now: time }))
             }
         } else {
         }
